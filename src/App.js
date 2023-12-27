56f097be58b3612e33909d20ab3dd38a303a9fea
Amiya Maity <amiyamaity.ee@gmail.com>
2023-12-28 01:32:41 +0530

    first code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Controlled from './src/Controlled'
+function App() {
+    return (
+        <div>
+            <Controlled/>
+        </div>
+    )
+}
+export default App
---
+++ src/Controlled.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+function Controlled() {
+    let [fname, setFName] = useState('');
+    let [lname, setLName] = useState('');
+
+    const handleFnameChange = (e) => {
+        setFName(e.target.value);
+    }
+    const handleLNameChange = (e) => {
+        setLName(e.target.value);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('form submitted successfully');
+    }
+    return (
+        <div>
+            <h1>Controlled</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={fname} onchange={handleFnameChange} name='fname' placeholder='First Name ' />
+                <input type="text" value={fname} onchange={handleLNameChange} name='lname' placeholder='Last Name ' />
+            </form>
+        </div>
+    )
+}
